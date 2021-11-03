456e4155afdf5d204fde4f7c86dd051f6bf2ce00
Colin-Kohli <colin.j.kohli@gmail.com>
2021-11-03 10:20:24 -0400

    Some light commenting

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,27 @@
 const axios = require('axios');
 
+// Link opened by clicking the icon if status is "play"
 var HOT_LINK = " ";
 let statusBarItem: vscode.StatusBarItem;
 
 import * as vscode from 'vscode';
 
+// Main extension function, called on VSCode startup completion
 export async function activate(context: vscode.ExtensionContext) {
 
+	// Create status bar icon
 	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
 	setStatus("wait");
 	context.subscriptions.push(statusBarItem);
 
+	// Register refresh command. Triggered by icon press or thru command pallette
 	let refresh = vscode.commands.registerCommand('chester.refresh', async () => {
 		await isYourTurn();
 		vscode.window.showInformationMessage('Games updated.');
 	});
 	context.subscriptions.push(refresh);
 
+	// Register openLink command. Opens HOT_LINK when triggered.
 	let openLink = vscode.commands.registerCommand('chester.open_link', async () => {
 		console.log(HOT_LINK);
 		let success = await vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(HOT_LINK));
@@ -24,21 +29,29 @@ export async function activate(context: vscode.ExtensionContext) {
 	});
 	context.subscriptions.push(openLink);
 
+	// Register errorInfo command. Display error message when icon is clicked in
+	// failed state.
 	let errorInfo = vscode.commands.registerCommand('chester.error_info', async () => {
-		var playerName = vscode.workspace.getConfiguration("chester").get("username");	
 		vscode.window.showInformationMessage( `
-		Error fetching games for user ` + playerName);
+		Error fetching games for user. Please double check personal API token is
+		added correctly.`);
 		isYourTurn();
 	});
 	context.subscriptions.push(errorInfo);
 
+	// Perform inital check
 	isYourTurn();
+
+	// Perform check on interval in extension configuration
 	setInterval(isYourTurn, vscode.workspace.getConfiguration('chester').get("refreshTimer"));
 }
 
+// Checks game status and sets icon status accordingly
 async function isYourTurn() {
 
 	console.log("Updating...");
+
+	// getGames() will return a game Id where it is your turn, if one exists.
 	const activeGame = await getGames();
 
 	if (activeGame === "error"){
@@ -55,6 +68,9 @@ async function isYourTurn() {
 	}
 }
 
+// Returns the Id of a game where it is your turn, if one exists. 
+// Returns "error" if any exception is thrown
+// Returns null if no games where it is your turn.
 async function getGames() {
 	let url = "https://lichess.org/api/account/playing";
 	let PAT = vscode.workspace.getConfiguration("chester").get("pat");
@@ -79,6 +95,7 @@ async function getGames() {
 	}
 }
 
+// Sets icon status and funtionality. 
 function setStatus(status: String){
 
 	console.log("Status set: " + status);
